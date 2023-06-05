9b9627bb637d0c9e331503d0370ea238c1b7c181
marea <m.arevalo.aragon@gmail.com>
2023-06-05 14:56:23 -0600

    Added config for lsp

---
+++ configs/lspconfig.lua
@@ -0,0 +1,24 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.clangd.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.omnisharp.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
--- plugins.lua
+++ plugins.lua
@@ -13,6 +13,13 @@ local plugins = {
         }
     end,
     lazy = false
-  }
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
 }
 return plugins
