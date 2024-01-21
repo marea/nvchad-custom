ab20fc54723e41cf60f572b28f52726a6fabe8cc
marea <m.arevalo.aragon@gmail.com>
2024-01-21 00:38:48 -0600

    Added plantuml previewer and deps

--- plugins.lua
+++ plugins.lua
@@ -35,6 +35,13 @@ local plugins = {
     event = {"CmdlineEnter"},
     ft = {"go", 'gomod'},
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    "weirongxu/plantuml-previewer.vim",
+    dependencies = {
+      "aklt/plantuml-syntax",
+      "tyru/open-browser.vim",
+    }
   }
 }
 return plugins
